ae6dc10672c6e98b820725d64a357b630a293382
bahadir <baha_i@hotmail.com>
2021-06-09 15:48:51 +0200

    to do list2

---
+++ main.js
@@ -0,0 +1,4 @@
+let addToDoButton=document.querySelector("#addToDo"); 
+let toDoContainer=document.querySelector("#toDoContainer");
+let inputText=document.querySelector("#inputText");
+let clearToDo=document.querySelector("#clearToDo");
